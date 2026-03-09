88c9e97a5901626cd2062dd9e554a5fc15ca0393
Sachin <100507127+sachinpal11@users.noreply.github.com>
2026-03-09 22:19:37 +0530

    feat:added import env

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: `${import.meta.env.BACKEND_URL}/api`,
   withCredentials: true,
 });
 
